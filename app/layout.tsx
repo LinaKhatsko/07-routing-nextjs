52cd87723151e7f0ff8121c0ca6e6301a62cdee1
Lina Khatsko <lina.khatsko@gmail.com>
2025-10-24 15:14:28 +0300

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -22,6 +24,7 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
           <main>{children}</main>
+          {modal}
           <Footer />
         </TanStackProvider>
       </body>
